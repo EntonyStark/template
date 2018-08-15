1b7c1e51985d9ed8419264ad1cb4facacc5f6e5f
Entony <tony@MacBook-Air-Entony.local>
2018-08-15 14:36:32 +0300

    finish first layout

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         <p className="container">
 					To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Link to="/test">Test</Link>
+        <Link to="/test-1">First Layout with ise float</Link>
       </div>
     );
   }
--- src/router.js
+++ src/router.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import App from './Containers/App';
-import TestComp from './Containers/test';
+import TestComp from './Containers/FirstLayout';
 
 const Routes = props => (
   <Switch {...props}>
     <Route exact path="/" component={App} />
-    <Route exact path="/test" component={TestComp} />
+    <Route exact path="/test-1" component={TestComp} />
   </Switch>
 );
 
